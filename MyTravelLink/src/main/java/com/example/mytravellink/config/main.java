f32f72f17b7ceb5fbcc696011bf325c3c6bd5559
Yuchan <yuchankim0906@gmail.com>
2025-01-21 19:37:14 +0900

    Deploy Test

--- MyTravelLink/src/main/java/com/example/mytravellink/config/main.java
+++ MyTravelLink/src/main/java/com/example/mytravellink/config/main.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyTravelLinkApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MyTravelLinkApplication.class, args);
+          SpringApplication.run(MyTravelLinkApplication.class, args);
     }
 }
