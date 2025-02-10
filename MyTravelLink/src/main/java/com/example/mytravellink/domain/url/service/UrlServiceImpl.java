1074a0e515bcbcdcba120de9be3217b5ece67024
DESKTOP-OKVQUTP\201-19 <qwes5674@gmail.com>
2025-02-10 10:08:21 +0900

    fastapi-spring 연결

--- MyTravelLink/src/main/java/com/example/mytravellink/domain/url/service/UrlServiceImpl.java
+++ MyTravelLink/src/main/java/com/example/mytravellink/domain/url/service/UrlServiceImpl.java
@@ -13,9 +13,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-
-import java.math.BigDecimal;
 import java.util.*;
 
 @Service
