dfc09c5b70ced06a8fe22962f8c19450d8c83703
Gun-code <dlgmlrnjs213@gmail.com>
2025-02-22 23:49:00 +0900

    fix: White Label 에러 해결을 위한 webConfig 설정정

--- MyTravelLink/src/main/java/com/example/mytravellink/config/WebConfig.java
+++ MyTravelLink/src/main/java/com/example/mytravellink/config/WebConfig.java
@@ -10,9 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // 예: /loginSuccess 요청이 들어오면 index.html로 포워딩
-        registry.addViewController("/loginSuccess")
-                .setViewName("forward:/index.html");
+        // Forward all routes to index.html
+        registry.addViewController("/**").setViewName("forward:/index.html");
 
         // 우선순위 설정 (선택)
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
